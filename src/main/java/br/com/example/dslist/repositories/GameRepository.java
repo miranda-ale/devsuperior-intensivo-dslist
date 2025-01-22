9c9c2e23b254f1528c72ce6b46706c0e961ee6c7
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-22 19:14:08 -0300

    Game repository, service and controller

--- src/main/java/br/com/example/dslist/repositories/GameRepository.java
+++ src/main/java/br/com/example/dslist/repositories/GameRepository.java
@@ -3,6 +3,8 @@ package br.com.example.dslist.repositories;
 import br.com.example.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Camada de acesso aos dados
+
 public interface GameRepository extends JpaRepository<Game, Long> {
 
 
